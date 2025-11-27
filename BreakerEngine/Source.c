16e4aa6c23250191d901edbd431e5b4e543d0534
Xanhos <grallanyann84@gmail.com>
2025-11-27 18:27:23 +0100

    Update Source.c

--- BreakerEngine/Source.c
+++ BreakerEngine/Source.c
@@ -4,16 +4,6 @@
 
 int main(void)
 {
-	Animation* anim = CreateAnimationFromFile("C:\\Users\\y.grallan\\Downloads\\PlayerPoisson.anim",NULL);
-
-	Animation* newanim = CopyAnimation(anim);
-	anim->SelectAnimationKey(anim, "IdleF");
-	anim->Update(anim, 0.016f);
-	anim->Destroy(&anim);
-	newanim->SelectAnimationKey(newanim, "IdleF");
-	newanim->Update(newanim, 0.016f);
-	newanim->Destroy(&newanim);
-
 
 	InitResourcesManager("../Ressources");
 	StartGame(CreateWindowManager(1920, 1080, "BreakerEngine", sfDefaultStyle, NULL), "MainMenu", "Loading", &ResetLoadingState);
